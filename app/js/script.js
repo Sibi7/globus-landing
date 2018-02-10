4f97437e0d26a4a3749af14a37f88f93bbe24f7f
Sibi7 <Sibi_7@mail.ru>
2018-02-10 21:46:23 +0300

    add left sidebar menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 
+    $('.owl-carousel-header').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay: true,
+        nav:false,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
 });
